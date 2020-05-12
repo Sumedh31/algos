8c3a4e3b706c08e602cdf697af8a5f7acdda3a17
Sumedh Tambe <sumedhrtv@gmail.com>
2020-05-12 09:03:05 +0200

    Implemented preorder,postorder traversal as well in iterative manner.

--- Trees/BinaryTreetraveserIterative.py
+++ Trees/BinaryTreetraveserIterative.py
@@ -7,7 +7,13 @@ Created on 07-May-2020
           /   \ 
          2     3 
        /  \ 
-      4    5   """
+      4    5  
+            10
+          /   \ 
+         8     2 
+       /  \   /
+      3    5 2
+       """
       Inorder 42513
             1. Traverse the left subtree, i.e., call Inorder(left-subtree)
             2. Visit the root.
@@ -21,6 +27,7 @@ Created on 07-May-2020
            2. Traverse the right subtree, i.e., call Postorder(right-subtree)
            3. Visit the root.
 '''
+
 class Node():
     def __init__(self,data):
         self.data=data
@@ -39,9 +46,39 @@ def IterativeInorderTraversal(root):
             currentRoot=currentRoot.right
         else:
             break
+def IterativePostOrder(root):
+    firststack=[]
+    secondstack=[]
+    
+    if root is None:
+        return
+    firststack.append(root)
+    while firststack:
+        current=firststack.pop()
+        secondstack.append(current)
+        if(current.left):
+            firststack.append(current.left)
+        if(current.right):
+            firststack.append(current.right)
+    while secondstack:
+        node=secondstack.pop()
+        print(node.data,end=" ")
+def IterativePreorder(root):
+    nodestack=[]
+    if root is None:
+        return
+    nodestack.append(root)
+    while(len(nodestack)>0):
+        current=nodestack.pop()
+        print(current.data,end=" ")
+        if(current.right is not None):
+            nodestack.append(current.right)
+        if(current.left is not None):
+            nodestack.append(current.left)
 root = Node(1) 
 root.left = Node(2) 
 root.right = Node(3) 
 root.left.left = Node(4) 
 root.left.right = Node(5) 
-IterativeInorderTraversal(root)
+IterativePostOrder(root)
+IterativePreorder(root)
